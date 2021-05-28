e33751ad3ba529d349b27df568e7e4d81c488db1
Michael Kelly <20041540@mail.wit.ie>
2021-05-28 13:17:20 +0100

    workaround markers netifly

--- src/pages/map.svelte
+++ src/pages/map.svelte
@@ -3,17 +3,6 @@
     import {LeafletMap} from '../services/leaflet-map';
     import {onMount, getContext} from "svelte";
     const poiService = getContext("PoiService");
-    import L from 'leaflet';
-    import {
-        LMap, LTileLayer, LMarker, LTooltip,
-    } from '../services/leaflet-map';
-
-    delete L.Icon.Default.prototype._getIconUrl;
-    L.Icon.Default.mergeOptions({
-        iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-        iconUrl: require('leaflet/dist/images/marker-icon.png'),
-        shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-    });
 
 
     let lat = 52.160858;
